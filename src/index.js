03ebabe42e86b520857cbcb213bbca3c657c5f97
kushal atreya <kushalatreya@live.com>
2018-11-14 14:31:06 +0200

    started from boilerplate code, now finished main layout and folding feature

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { render } from "react-dom";
+import "./components/StorePicker.js";
+import App from "./components/App";
+import "./css/style.css";
+
+render(<App />, document.querySelector("#main"));
