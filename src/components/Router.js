cbadf861364d261038d02a7ce7f6b2204cc36760
kushal atreya <kushalatreya@live.com>
2018-11-16 10:04:32 +0200

    3 paths to route, rendered from index.js

---
+++ src/components/Router.js
@@ -0,0 +1,20 @@
+//even router in react is a component.. enjoying so far
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import StorePicker from './StorePicker';
+import App from './App';
+import NotFound from './NotFound';
+
+const Router = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path='/' component={StorePicker} />
+                <Route path='/store/:storeId' component={App} />
+                <Route component={NotFound} />
+            </Switch>
+        </BrowserRouter>
+    )
+};
+
+export default Router;
