62d02b3de391778dedebf7d11544ccfc6bf4efae
kushal atreya <kushalatreya@live.com>
2018-11-16 10:02:11 +0200

    will include all the list of items available

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React from 'react';
 
-class Inventory extends React.Component {
-  render() {
-    return <div className="inventory">Inventory!!!</div>;
-  }
+class Inventory extends React.Component{
+    render(){
+        return(
+            <div className="inventory">
+               inventories here
+            </div>
+        )
+    }
 }
 
 export default Inventory;
