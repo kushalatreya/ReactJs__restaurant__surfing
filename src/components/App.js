7d300d309d796eb0c0bcd16122baf9c346e17518
kushal atreya <kushalatreya@live.com>
2018-11-16 10:00:50 +0200

    working with header props, do inventory and order

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,29 @@
-import React from "react";
-import Header from "./Header";
-import Order from "./Order";
-import Inventory from "./Inventory";
+import React from 'react';
+import Header from './Header'
+import Order from './Order'
+import Inverntory from './Inventory'
 
 class App extends React.Component {
-  render() {
-    return (
-      <div className="catch-of-the-day">
-        <div className="menu">
-          <Header tagline="Fresh Seafood Market" age={100} />
-          
-        </div>
-        <Order />
-        <Inventory />
-      </div>
-    );
-  }
-}
+    render(){
+        return(
+            <React.Fragment>
+                <div className="catch-of-the-day">
+                    <div className="menu">
+                        {/* if you want to pass anything other than string, you have to use {} */}
+                        <Header tagline='this is cool as rock'  />
+                        
+
+                    </div>
+                    <Inverntory/>
+                    <Order/>
+                </div>
+            </React.Fragment>
+        )
+    }
+
+
+
+
+};
 
 export default App;
