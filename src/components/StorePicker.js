8709efc841da9c0205f1dee086670a98be805961
kushal atreya <kushalatreya@live.com>
2018-11-16 10:05:43 +0200

    renders random store name from helper.js, event handler to work on

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,25 @@
 import React from "react";
+import {getFunName} from  '../helpers'
+
 
 class StorePicker extends React.Component {
+  
+  handleStore(event){
+    //1. stop the page from submitting 
+    event.preventDefault();
+    console.log('handleStore is working')
+    //2. get the text from corresponding input fields
+    
+    //3. change the page to /store/:storeId
+  }
+  
+  
+  
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit= {this.handleStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
         <button type="submit">Visit Store →</button>
       </form>
     );
