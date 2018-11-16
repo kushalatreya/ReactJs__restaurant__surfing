41d7ed6c20ea7b38edb80fd78fb152f939d9452c
kushal atreya <kushalatreya@live.com>
2018-11-16 10:01:32 +0200

    added dynamic props

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,56 @@
-import React from "react";
+import React from 'react';
 
-const Header = props => (
-  <header className="top">
-    <h1>
-      Catch
-      <span className="ofThe">
-        <span className="of">of</span>
-        <span className="the">The</span>
-      </span>
-      Day
-    </h1>
-    <h3 className="tagline">
-      <span>{props.tagline}</span>
-    </h3>
-  </header>
-);
+const Header = ({tagline}) => {
+    //de-structuring props into ({tagline}) direct variable so, we don't have to use props.tagline below.
+    return (
+        <header className="top">
+            <h1>
+                Catch
+                    <span className="ofThe">
+                    <span className="of">of</span>
+                    <span className="the">the</span>
+                </span>
+                day</h1>
+            <h3 className="tagline">
+                <span>{tagline}</span>
 
+            </h3>
+        </header>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+class Header extends React.Component{
+    render(){
+        return (
+            <header className="top">
+                <h1>
+                    Catch
+                    <span className="ofThe">
+                     <span className="of">of</span> 
+                     <span className="the">the</span>
+                    </span> 
+                     day</h1>
+            <h3 className="tagline">
+                    <span>{this.props.tagline}</span>
+                
+            </h3>
+            </header>
+        )
+    }
+}; */
 export default Header;
